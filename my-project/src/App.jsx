736bb4629ea866c8adee2ecb5cb3762820d2510d
Raven Khaya <rkhaya1998@gmail.com>
2024-04-24 14:15:22 -0500

    analytics

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar"
 import About from "./components/About"
 import Technologies from "./components/Technologies"
 import Experience from "./components/Experience"
+import { Analytics } from "@vercel/analytics/react"
 import Projects from "./components/Projects"
 import Contact from "./components/Contact"
 
@@ -19,6 +20,7 @@ const App = () => {
       <Technologies/>
       <Projects/> 
       <Contact/>
+      <Analytics/>
       </div>
     </div>
   )
